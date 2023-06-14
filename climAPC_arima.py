a66abcd7e41fae37d51e915b41c59fe073898cbf
tcknoop <108334115+tcknoop@users.noreply.github.com>
2023-06-15 01:13:14 +0200

    Add files via upload
    this

--- climAPC_arima.py
+++ climAPC_arima.py
@@ -5,7 +5,7 @@ import scipy as sc
 from helpfuncs import * 
 from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.graphics.tsaplots import plot_pacf, plot_acf
-     
+props = dict(boxstyle='square', facecolor='lightgrey',edgecolor = 'red', alpha=0.9)
 
 
 params = {'legend.fontsize': 'x-large',
@@ -34,16 +34,41 @@ time = t_series[:,0]
 nan_index = [2,10,11,12]
 data2 = data.copy()
 data2[nan_index] = np.nan
+order = (1,1,1)
 #%%
 
-def examine_data(data,time, d = 0):
+def examine_data(data,time, d = 0, warnings = True):
+    '''
     
-    #TODO
-    #plot to determine stationarity?
+
+    Parameters
+    ----------
+    data : 1-D array-like
+        Input timeseries.
+    time : 1-D array-like
+        Input time vector, same length as data.
+    d : int, optional
+        Order of differencing to examine possible higher orders of d. The default is 0.
+    warnings : bool, optional
+        prints possible warnings into the first plot. The default is True.
+
+    Raises
+    ------
+    ValueError
+        For not matching dimension between data and time.
+
+    Returns
+    -------
+    None.
+
+    '''
     
     if data.shape != time.shape:
         raise ValueError(f"Timeseries data and time vector should have identical dimensions, not {data.shape} and {time.shape} ")
     
+    warn = check_for_errors(data, time)
+    if warnings == False:
+        warn = None
     #differentiate d times
     if d > 0:
         data,time = difference(data,time, d = d)
@@ -55,14 +80,19 @@ def examine_data(data,time, d = 0):
     ax.plot(time,data)
     ax.set_xlabel('Time')
     ax.set_title(f'Timeseries d={d} times differentiated')
+    ax.text(0.05, 0.95, warn, transform=ax.transAxes, fontsize=10,
+            verticalalignment='top', bbox=props)
     ax.grid()
 
     ax = fig.add_subplot(3,1,2)
     plot_acf(data, ax = ax)
     ax.set_xlabel('Lag')
+    ax.set_ylabel('ACF')
     ax.grid()
+    
     ax = fig.add_subplot(3,1,3)
     plot_pacf(data, ax = ax)
+    ax.set_ylabel('PACF')
     ax.set_xlabel('Lag')
     ax.grid()
 
@@ -71,24 +101,55 @@ def examine_data(data,time, d = 0):
 
 #examine_data(data, time, d = 2)
 
-def interp_data(data, time , method):
+def fix_data(data, time , method):
     #TODO
     #different methods:
         #cut NANs
         #linear
         #seasonal?
     
-    return None
+    return data, time
 
 def construct_ARIMA(data,time,
                     order = (1,1,1),#AR(q),I(d),MA(p)
-                    season = None,
+                    season = (0,0,0,0),# (Q,D,P,s)
                     manual = True,
-                    plot_result = True
+                    plot_result = True,
+                    ret_aic = False
                     ):
+    '''
+    
+
+    Parameters
+    ----------
+    data : 1-D array-like
+        Input timeseries.
+    time : 1-D array-like
+        Input time vector, same length as data.
+    order : tuple (int,int,int) , optional
+        (p,d,q) Parameters in the ARIMA model. p for autoregressive model AR(p);
+        d for the order of integration; q for the moving-average model MA(q). Default is (1,1,1).
+    season: bool or tuple:
+       expands the model to a SARIMA model. must include the seasonal parameters (Q,D,P,s);
+       s indicates the number of timesteps per year (e.g. 12 for monthly timeseries).
+    manual : bool, optional
+        wether to manually determine model parameters or let the statsmodels library. The default is True.
+    plot_result : bool, optional
+        DESCRIPTION. The default is True.
+
+    Returns
+    -------
+    result : TYPE
+        fitted ARIMA model object; can be parsed to the forecast function
+
+    '''
     #TODO add seasonal
     #construct ARIMA model
-    model = ARIMA(data, order = order)
+    warn = check_for_errors(data, time)
+    
+    model = ARIMA(data, order = order,
+                  seasonal_order = season,
+                  missing = 'raise')
     
     #fit
     result = model.fit()
@@ -97,33 +158,38 @@ def construct_ARIMA(data,time,
     if plot_result:
         fig = plt.figure(figsize=(6,8))
     
-        ax = fig.add_subplot(2,1,1)
+        ax = fig.add_subplot(3,1,1)
         ax.plot(time,data, label = 'Data')
         ax.plot(time,result.fittedvalues, label = 'Fitted Model')
         ax.legend()
         
-        ax = fig.add_subplot(2,1,2)
+        ax = fig.add_subplot(3,1,2)
         ax.plot(time,result.resid)
         ax.set_title('Residuals')
+        
+        ax = fig.add_subplot(3,1,3)
+        ax.plot(time,result.resid)
+        ax.set_title('Autocorrelation of Residuals')
+        ax.set_xlabel('Lags')
+        ax.set_ylabel('ACF')
+        
+        plt.tight_layout()
     
     #TODO print RMSE, or AIC to compare
     #maybe parse on to automate finding best model
     return result
 
-def forecast_ARIMA(model):
-    #TODO forecast here
-    print('nothing to see here yet')
-    #plot with confidence interv
-    return None
+def forecast_ARIMA(result, steps, confidence = None, plot_results = True):
+    pred = result.forecast(steps, alpha = confidence)
+
+    plt.plot(data, label= 'data')
+    plt.plot(len(data)+np.arange(pred.shape[0]),pred,lw = 3, label = 'forecast')
+    plt.legend()
+    return pred
 
 
 #res = construct_ARIMA(data, time, order = (7,1,1))
-#%%
-
-
-
-
-
+#%% test
 
 
 
--- helpfuncs.py
+++ helpfuncs.py
@@ -6,23 +6,82 @@ from scipy import stats
 import warnings
 #%%
 
-def check_interp(array,time):
-    nan_count = np.isnan(array).sum()
-    if nan_count > 0:
-        warning_message = f"{nan_count} NaN Value(s) detected in timeseries; interpolation or deletion of missing values advised."
-        warnings.warn(warning_message,stacklevel = 2 )
-    #TODO #maybe include in     
-    #check timestep continuity
-    
-    #check Value errors over std?; over intg std?
-    
-    return None
 
-def difference(x,t,d):
-    
+
+def difference(x, t = None, d = 1):
+    '''
+    Takes the difference of the timeseries x a total of d times. Functions as the "I" part of the ARIMA model.
+
+    Parameters
+    ----------
+    x : 1-D array:
+        Timeseries data; same dimension as t
+    t : 1-D array, optional:
+        Timevector, same dimension as x.
+    d : int
+        order of differencing; d=0 returns unchanged vectors.
+
+    Returns
+    -------
+    x : 1-D array;
+        Differenced timeseries; shortened by d indeces from the front.
+    t : 1-D array;
+        Differenced timevector; shortened by d indeces from the front.
+
+    '''
+    #take difference of data d times
     for i in range(d):
         x = np.ediff1d(x)
     
-    return x,t[d:]
+    if t is  None:
+        return x
+    else:
+        return x,t[d:] #slice timevector to same dimensions 
+
+
+def check_for_errors(array,time):
+    '''
+    Checks the Timeseries for Nans, missing data, uneven timesteps or extreme discontinuities.
+
+    Parameters
+    ----------
+    data : 1-D array-like
+        Input timeseries.
+    time : 1-D array-like
+        Input time vector, same length as data.
+
+    Returns
+    -------
+    None.
+
+    '''
+    err_found = 0
+    warning_message = None
+    nan_count = np.isnan(array).sum() #count NaNs
+    if nan_count > 0:
+        warning_message = f"{nan_count} NaN Value(s) detected in timeseries;\ninterpolation or deletion of missing values advised."
+        warnings.warn(warning_message,stacklevel = 2 )
+        err_found =+ 1
+        
+    diff = difference(time)
+    if np.amax(diff)>np.mean(diff)+0.001: #check for inconsistency in timestep; allow for float error
+        warning_message = 'Irregular timesteps found; filling missing timesteps with NaN\nor Interpolation to regular timesteps advised'
+        warnings.warn(warning_message,stacklevel = 2 )
+        err_found =+ 1
+    
+    #check if any indices lie way outside the standard deviation -> discontinuities
+    z_score = (array - np.mean(array)) / np.std(array)
+    is_outlier = np.abs(z_score) > 3
+    outlier_indices = np.where(is_outlier)[0].tolist()
+    if outlier_indices:
+        warning_message = f'Outlier datapoint(s) found at {outlier_indices},\nmaybe delete or set to NaN?'
+        warnings.warn(warning_message,stacklevel = 2 )
+        err_found =+ 1
+
+    if err_found == 0:
+        print('no apparent errors found')
+        return None
+    if err_found >= 1:
+        return warning_message
 
 #%%
